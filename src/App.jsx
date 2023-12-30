7fdf08cf2ea18dc7783ee11a17711978fabd48de
AnesHamdaoui <bacetd2004@gmail.com>
2023-12-30 12:20:27 +0100

    style the changed btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [clickElement, setClickElement] = useState("Save Change");
-
+  const [changedClass, setChangedClass] = useState("save-btn");
   return (
     <button
       onClick={() => {
         setClickElement(<div className="loader"></div>);
         setTimeout(() => {
           setClickElement("Change Saved");
+          setChangedClass("save-btn changed-btn");
         }, 2000);
       }}
-      className="save-btn"
+      className={changedClass}
     >
       {clickElement}
     </button>
